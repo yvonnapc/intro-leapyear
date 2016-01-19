e98e1d4952d6547d5499bc6e639f8a636ab27b37
Michael Lor and Yvonna Contreras <michael.vang.1985@gmail.com>
2016-01-19 08:50:54 -0800

    Add non-leap year funtionality

---
+++ js/scripts.js
@@ -0,0 +1,3 @@
+var leapYear = function(year) {
+  return false; 
+}
---
+++ spec/specs.js
@@ -0,0 +1,5 @@
+describe('leapYear', function() {
+  it("is false for a year that is not divisible by 4, 100, or 400", function() {
+    expect(leapYear(1993)).to.equal(false);
+  });
+});
