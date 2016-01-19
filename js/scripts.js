3e727ba641ec7667caa061e943eb0e0efc64af22
Michael Lor and Yvonna Contreras <michael.vang.1985@gmail.com>
2016-01-19 09:04:55 -0800

    commit change to simplify code

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,7 @@
 var leapYear = function(year) {
-  if (year % 100===0) {
-    return false;
-  } else if (year % 4===0){
+  if ((year % 4===0) && (year % 100 !==0)) {
     return true;
   } else {
     return false;
-  }
+ }
 };
